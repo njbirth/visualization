34398ec0f1206bd9ab99d8530e66e41230500e90
Ole Umlauft <ole.umlauft@protonmail.com>
2021-07-16 01:56:03 +0200

    Change directory

---
+++ website/src/bundestag/Wahlperiode.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let wp = 19;
+  export let von = "24.10.2017";
+  export let bis = null;
+  export let liste = "SH";
+  export let mandatsart = "Landesliste";
+</script>
+
+<article class="box">
+  <p class="title">Wahlperiode {wp}</p>
+  Dummy
+</article>
+
+<style>
+  .box {
+    border-radius: 5px;
+    box-shadow: 1px 3px 3px 3px lightgray;
+    height: 10em;
+    padding: 0.1em 1em 1em 1em;
+    margin: 0.1em 1em 1em 1em;
+  }
+
+  .title {
+    font-weight: bold;
+    font-size: 18px;
+  }
+</style>
